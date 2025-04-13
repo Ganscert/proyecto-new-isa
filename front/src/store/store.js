3450ba5a065fcaea34f1ed884dd8a833fe1bac91
emmanuel <emmanuel2929xl@gmail.com>
2025-04-13 13:43:23 -0400

    cambios en preparacion

--- front/src/store/store.js
+++ front/src/store/store.js
@@ -1,9 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authSlice } from "./auth/authSlice";
 
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
+// Configuración de persistencia
+const persistConfig = {
+  key: 'auth',
+  storage,
+};
+
+// Reducer persistido para auth
+const persistedAuthReducer = persistReducer(persistConfig, authSlice.reducer);
+
+// Crear el store usando el reducer persistido
 export const store = configureStore({
   reducer: {
-    auth: authSlice.reducer
+    auth: persistedAuthReducer, // aquí usas el persistido
   },
 })
